f630e268ed0d6923c0cce192226ae395e6eb5fd3
Eman <emanmokhtar2020@gmail.com>
2015-04-07 04:54:16 +0200

    create product category and sub category and subsubcategory

---
+++ ERP_Project/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name':'stockee',
+    'version':'1.1',
+    'summary': 'برنامج ادارة جميع المخازن',
+    'description': """
+    يساعد هذا البرنامج لتسهيل عملية ادارة جميع المخازن
+    """ ,
+    'author':'طلاب معد تكنولوجيا المعلومات',
+    'depends':['base'],
+    'data': [
+        'ERP_Project_view.xml',
+    ],
+    'installable':'true',
+
+}
