ed0d880b31dd5dcd0b1a3d95ac1109504d4f8ea7
zoza <marthasoliman966@yahoo.com>
2015-04-09 17:18:18 +0200

    security and report for mystock

--- ERP_Project/__openerp__.py
+++ ERP_Project/__openerp__.py
@@ -8,7 +8,8 @@
     'description':"""""",
     'website':'http://www.iti.gov.eg',
     'depends':['base','hr','report_webkit'],
-    'data':['ERPProject_view.xml','ERP_Project_view.xml','reportproduct/report.xml'], #xml files
+    'data':['ERPProject_view.xml','ERP_Project_view.xml','reportproduct/report.xml','reportmystock/report.xml',
+		'security/ERP_Project_security.xml','security/ir.model.access.csv'], #xml files
     'installed':True, #ynf3 eno ytstab
     'auto-install':False, #ytstab mno l nafso
 }
